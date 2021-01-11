92dcd246ad40b8658bcb64ed9abd4187e96e2357
Al Akdari <alkan.akdari@gmail.com>
2021-01-10 23:40:46 -0500

    Pictures can be viewed!

--- LetsDrink/Controllers/HomeViewController.swift
+++ LetsDrink/Controllers/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 class HomeViewController: UIViewController{
     
@@ -20,8 +21,9 @@ class HomeViewController: UIViewController{
     //Mark - View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -29,6 +31,13 @@ class HomeViewController: UIViewController{
                self.posts = posts
                self.tableView.reloadData()
         }
+    }
+    
+    func configureTableView() {
+        // remove separators for empty cells
+        tableView.tableFooterView = UIView()
+        // remove separators from cells
+        tableView.separatorStyle = .none
     }
 }
 
@@ -40,9 +49,23 @@ extension HomeViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
-                cell.backgroundColor = .red
+        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath) as! PostImageCell
+        
+        let imageURL = URL(string: post.imageURL)
+        cell.postImageView.kf.setImage(with: imageURL)
+        
         return cell
+    }
+}
+
+// MARK: - UITableViewDelegate - make this compatible with the tableviewDelegate
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+
+        return post.imageHeight
     }
 }
 
---
+++ LetsDrink/Controllers/PostImageCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostImageCell.swift
+//  LetsDrink
+//
+//  Created by Al Akdari on 2021-01-10.
+//  Copyright © 2021 AlCo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    //Mark subservices
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
